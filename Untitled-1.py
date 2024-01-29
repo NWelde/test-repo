fed816d1adb9f10c3e66ce4199cc9a501dd73885
kenerian <95562737+Anonymous-kd@users.noreply.github.com>
2024-01-29 20:02:24 +0300

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,25 @@
+#simple calculator
+try :
+    x = int(input('Enter num: '))
+    y =int(input('enter num2: '))
+except :
+   print('i want numbers dumbass, try again')   
+   x = int(input('Enter num: '))
+   y =int(input('enter num2: '))
+
+try :
+    z =str(input('Enter operator: '))
+except:
+ print('do you not know what an operator is?, try again man.')    
+
+if z == '+' :
+ print(x+y)
+elif z == '-' :
+   print(x-y)
+elif z == '*' :
+ print(x*y)
+elif z == '/' :
+ print(x/y)
+
+
+
